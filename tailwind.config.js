e3f202184136fab8096451fcf8cb0005388b9340
Sean Irby <sean@rivaliq.com>
2023-02-08 07:40:22 -0800

    update tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       'roboto': [ 'Roboto', 'sans-serif'],
